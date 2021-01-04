42d6b1782a3cac4541afab2c3ea3525d6ad37705
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2021-01-04 13:38:42 -0300

    Multiple Actions

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -11,13 +11,20 @@ const initialState = {
 }
 /* Estado precisa ser imutável, assim não posso editar o estado existente ou anterior
 ... para copiar o estado anterior */
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.ShoppingListActions) {
   switch(action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload]
       }
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      } //copia os valores antigos e adiciona os novos
     default: //usado por exemplo no carregamento inicial
       return state;
   }
